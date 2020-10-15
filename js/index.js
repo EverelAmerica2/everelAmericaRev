1e054b353073f7e32543bb8dbabb714252d4b165
everelamerica2 <everelamerica2@everelamerica.com>
2020-10-15 10:20:52 -0400

    finish grid in news page

--- js/index.js
+++ js/index.js
@@ -59,21 +59,21 @@ const printNavigation = function(){
     }
     $('.header-nav').empty();
     $('.header-nav').append(`
-    <a href="index.html" class="header-nav-item">Home</a>
-                <a href="${prefix}about.html" class="header-nav-item">About the Company</a>
-                <a href="${prefix}products.html" class="header-nav-item">Products</a>
-                <a href="${prefix}distribution.html" class="header-nav-item">Sales Network</a>
-                <a href="${prefix}contact.html" class="header-nav-item">Contact Us</a>
+        <a href="${prefix}index.html" class="header-nav-item">Home</a>
+        <a href="${prefix}about.html" class="header-nav-item">About the Company</a>
+        <a href="${prefix}products.html" class="header-nav-item">Products</a>
+        <a href="${prefix}distribution.html" class="header-nav-item">Sales Network</a>
+        <a href="${prefix}contact.html" class="header-nav-item">Contact Us</a>
     `);
 
     $('.navigation__nav').empty();
     $('.navigation__nav').append(`
     <ul class="navigation__list">
-    <li class="navigation__item"><a href="${prefix}index.html" class="navigation__link" ><span>01</span>Home</a></li>
-    <li class="navigation__item"><a href="${prefix}about.html" class="navigation__link" ><span>02</span>About Us</a></li>
-    <li class="navigation__item"><a href="${prefix}products.html" class="navigation__link" ><span>03</span>Products</a></li>
-    <li class="navigation__item"><a href="${prefix}distribution.html" class="navigation__link" ><span>04</span>Sales Network</a></li>
-    <li class="navigation__item"><a href="${prefix}contact.html" class="navigation__link" ><span>05</span>Contact Us</a></li>
+        <li class="navigation__item"><a href="${prefix}index.html" class="navigation__link" ><span>01</span>Home</a></li>
+        <li class="navigation__item"><a href="${prefix}about.html" class="navigation__link" ><span>02</span>About Us</a></li>
+        <li class="navigation__item"><a href="${prefix}products.html" class="navigation__link" ><span>03</span>Products</a></li>
+        <li class="navigation__item"><a href="${prefix}distribution.html" class="navigation__link" ><span>04</span>Sales Network</a></li>
+        <li class="navigation__item"><a href="${prefix}contact.html" class="navigation__link" ><span>05</span>Contact Us</a></li>
     </ul>
     `);
 }
